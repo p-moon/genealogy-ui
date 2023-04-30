727bb7c701d1e0ae71c9cf178f68b5fdaccfeabf
p-moon <moon@jdk.plus>
2023-04-30 20:34:39 +0800

    将新节点的操作单独封装一个模块，用于后续自定义使用

---
+++ src/components/GraphNodeProfile.vue
@@ -0,0 +1,105 @@
+<template>
+  <GraphNodeEditor ref="graphNodeEditor"></GraphNodeEditor>
+  <div v-if="isShowNodeMenuPanel" v-bind:class="{ 'node-menu-panel': true }"
+       v-bind:style="{left: nodeMenuPanelPosition.x + 'px',top: nodeMenuPanelPosition.y + 'px'}">
+    <div>对这个节点进行操作：</div>
+    <el-button type="primary" @click.stop="editNode">
+      <el-icon>
+        <Edit />
+      </el-icon>
+      编辑
+    </el-button>
+    <el-button type="primary" @click.stop="addNode">
+      <el-icon>
+        <CirclePlusFilled />
+      </el-icon>
+      添加
+    </el-button>
+    <el-button type="danger" @click.stop="deleteNode">
+      <el-icon>
+        <DeleteFilled />
+      </el-icon>
+      删除
+    </el-button>
+  </div>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent, onMounted, ref, watch, watchEffect } from "vue";
+import { Line } from "@/storage/model/Line";
+import { RelationGraphData, State, store } from "@/store";
+import { Node } from "@/storage/model/Node";
+import { CircleCloseFilled, CirclePlusFilled, DeleteFilled, Edit } from "@element-plus/icons-vue";
+import { relationGraphStorage } from "@/storage/RelationGraphStorge";
+import GraphNodeEditor from "@/components/GraphNodeEditor.vue";
+
+export default defineComponent({
+  name: "GraphNodeProfile",
+  components: { GraphNodeEditor, Edit, CircleCloseFilled, DeleteFilled, CirclePlusFilled },
+
+  setup() {
+    const isShowNodeMenuPanel = ref(false); // 是否展示操作菜单
+    let currentNode: Node = { id: "", text: "" }; // 当前操作的节点
+    const nodeMenuPanelPosition = ref({ x: 0, y: 0 }); // 操作菜单位置
+    let graphNodeEditor = ref<InstanceType<typeof GraphNodeEditor>>();
+
+    function showNodeProfile(node: Node, x: number, y: number) {
+      isShowNodeMenuPanel.value = true;
+      nodeMenuPanelPosition.value.x = x;
+      nodeMenuPanelPosition.value.y = y;
+      currentNode = node;
+    }
+
+    function hideNodeProfile() {
+      isShowNodeMenuPanel.value = false;
+    }
+
+    function deleteNode() {
+      relationGraphStorage.deleteNode(currentNode);
+    }
+
+    function addNode(): Node {
+      const newNode: Node = { id: "new-node", text: "new-node", borderColor: "yellow" };
+      relationGraphStorage.addGraphNode(newNode);
+      const line: Line = { from: currentNode.id, to: newNode.id, text: "节点关系描述" };
+      relationGraphStorage.addLine(line); // 添加当前节点到新增子节点的关系表示
+      // showEditorDrawer(newNode);
+      return newNode;
+    }
+
+    function editNode(): Node {
+      showEditorDrawer(currentNode);
+      return currentNode;
+    }
+
+    function showEditorDrawer(node: Node) {
+      isShowNodeMenuPanel.value = false;
+      console.log("showNodeEditor", graphNodeEditor.value)
+      graphNodeEditor.value?.showNodeEditor(node);
+    }
+
+    return {
+      store,
+      currentNode,
+      nodeMenuPanelPosition,
+      isShowNodeMenuPanel,
+      graphNodeEditor,
+      deleteNode,
+      addNode,
+      editNode,
+      hideNodeProfile,
+      showNodeProfile,
+    };
+  },
+});
+</script>
+<style scoped>
+.node-menu-panel {
+    padding: 10px;
+    background-color: #ffffff;
+    border: #eeeeee solid 1px;
+    box-shadow: 0 0 8px #cccccc;
+    position: absolute;
+    z-index: 999;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,35 +7,7 @@
         </template>
       </RelationGraph>
     </div>
-    <div v-if="isShowNodeMenuPanel" v-bind:class="{ 'node-menu-panel': true }"
-         v-bind:style="{left: nodeMenuPanelPosition.x + 'px',top: nodeMenuPanelPosition.y + 'px'}">
-      <div>对这个节点进行操作：</div>
-      <el-button type="primary" @click.stop="editNode">
-        <el-icon>
-          <Edit />
-        </el-icon>
-        编辑
-      </el-button>
-      <el-button type="primary" @click.stop="addNode">
-        <el-icon>
-          <CirclePlusFilled />
-        </el-icon>
-        添加
-      </el-button>
-      <el-button type="danger" @click.stop="deleteNode">
-        <el-icon>
-          <DeleteFilled />
-        </el-icon>
-        删除
-      </el-button>
-      <el-button type="primary" @click.stop="closeMenu">
-        <el-icon>
-          <CircleCloseFilled />
-        </el-icon>
-        关闭
-      </el-button>
-    </div>
-    <GraphNodeEditor ref="graphNodeEditor"></GraphNodeEditor>
+    <GraphNodeProfile ref="graphNodeProfile"></GraphNodeProfile>
     <GraphLineEditor ref="graphLineEditor"></GraphLineEditor>
   </div>
 </template>
@@ -45,11 +17,8 @@ import { onMounted, ref, watch } from "vue";
 import RelationGraph, { RGJsonData } from "relation-graph/vue3";
 import { RelationGraphData, store } from "@/store";
 import { Node } from "@/storage/model/Node";
-import { Line } from "@/storage/model/Line";
-import { DeleteFilled, Edit, CirclePlusFilled, CircleCloseFilled } from "@element-plus/icons-vue";
-import { relationGraphStorage } from "@/storage/RelationGraphStorge";
 import relationGraphConfig from "@/config/RelationGraphConfig";
-import GraphNodeEditor from "@/components/GraphNodeEditor.vue";
+import GraphNodeProfile from "@/components/GraphNodeProfile.vue";
 import GraphLineEditor from "@/components/GraphLineEditor.vue";
 import { RGLine, RGLink } from "relation-graph/vue3/RelationGraph";
 
@@ -67,21 +36,20 @@ onMounted(() => {
 
 const myPage = ref<HTMLElement>();
 const relationGraph = ref<RelationGraph>();
-const graphNodeEditor = ref<InstanceType<typeof GraphNodeEditor>>();
 const graphLineEditor = ref<InstanceType<typeof GraphLineEditor>>();
+const graphNodeProfile = ref<InstanceType<typeof graphNodeProfile>>();
 
 const options = relationGraphConfig;
 
-const isShowNodeMenuPanel = ref(false); // 是否展示操作菜单
 let currentNode: Node; // 当前操作的节点
 const nodeMenuPanelPosition = ref({ x: 0, y: 0 }); // 操作菜单位置
 
 function onNodeClick(node: Node, $event: MouseEvent | TouchEvent):boolean {
   currentNode = node;
   const _base_position = myPage.value?.getBoundingClientRect();
-  isShowNodeMenuPanel.value = true;
-  nodeMenuPanelPosition.value.x = $event?.clientX - _base_position.x;
-  nodeMenuPanelPosition.value.y = $event?.clientY - _base_position.y;
+  let x = $event?.clientX - _base_position.x;
+  let y = $event?.clientY - _base_position.y;
+  graphNodeProfile.value?.showNodeProfile(currentNode, x, y);
   return true;
 }
 
@@ -91,30 +59,7 @@ function onLineClick(line: RGLine, link: RGLink, e: MouseEvent | TouchEvent):boo
 }
 
 function closeMenu() {
-  isShowNodeMenuPanel.value = false;
-}
-
-function showEditorDrawer(node: Node) {
-  isShowNodeMenuPanel.value = false;
-  graphNodeEditor.value?.showNodeEditor(node);
-}
-
-function deleteNode() {
-  relationGraphStorage.deleteNode(currentNode);
-}
-
-function addNode(): Node {
-  const newNode:Node = {id: 'new-node', text: 'new-node', borderColor: "yellow"};
-  relationGraphStorage.addGraphNode(newNode);
-  const line:Line = {from: currentNode.id, to: newNode.id, text: "节点关系描述"};
-  relationGraphStorage.addLine(line); // 添加当前节点到新增子节点的关系表示
-  // showEditorDrawer(newNode);
-  return newNode;
-}
-
-function editNode():Node {
-  showEditorDrawer(currentNode);
-  return currentNode;
+  graphNodeProfile.value.hideNodeProfile()
 }
 
 function buildShowData(graphData: RelationGraphData): RelationGraphData {
@@ -125,14 +70,6 @@ function buildShowData(graphData: RelationGraphData): RelationGraphData {
 
 </script>
 <style scoped>
-.node-menu-panel {
-    padding: 10px;
-    background-color: #ffffff;
-    border: #eeeeee solid 1px;
-    box-shadow: 0 0 8px #cccccc;
-    position: absolute;
-    z-index: 999;
-}
 .page-content {
     margin-right: 300px;
     border: #efefef solid 1px;
