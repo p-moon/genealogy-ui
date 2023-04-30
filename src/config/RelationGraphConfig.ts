21070e0387b732b7b131ec53a9af92bcbb87781a
p-moon <moon@jdk.plus>
2023-04-30 22:51:08 +0800

    调整布局相关逻辑

--- src/config/RelationGraphConfig.ts
+++ src/config/RelationGraphConfig.ts
@@ -1,6 +1,30 @@
-import { RGOptions } from "relation-graph/vue3/RelationGraph";
+import {
+  RGCenterLayoutOptions,
+  RGLayoutOptionsCore,
+  RGOptions,
+  RGTreeLayoutOptions
+} from "relation-graph/vue3/RelationGraph";
 
-const relationGraphConfig:RGOptions = {
+const centerLayout: RGCenterLayoutOptions = {
+  label: "中心",
+  layoutName: "center",
+  layoutClassName: "seeks-layout-center",
+  distance_coefficient: 1
+};
+
+const treeLayout: RGTreeLayoutOptions = {
+  label: "树状",
+  layoutName: "tree",
+  layoutClassName: "seeks-layout-center"
+};
+
+const forceLayout: RGLayoutOptionsCore = {
+  label: "自动",
+  layoutName: "force",
+  layoutClassName: "seeks-layout-center"
+};
+
+const relationGraphConfig: RGOptions = {
   allowSwitchLineShape: true,
   allowSwitchJunctionPoint: true,
   defaultJunctionPoint: 'border',
@@ -11,25 +35,11 @@ const relationGraphConfig:RGOptions = {
   useAnimationWhenRefresh: true,
   allowShowZoomMenu: true,
   placeSingleNode: true,
-  // layouts: [
-  //   {
-  //     label: "中心",
-  //     layoutName: "center",
-  //     layoutClassName: "seeks-layout-center",
-  //     distance_coefficient: 1
-  //     // defaultJunctionPoint: 'border'
-  //   },
-  //   {
-  //     label: "树状",
-  //     layoutName: "tree",
-  //     layoutClassName: "seeks-layout-center",
-  //   },
-  //   {
-  //     label: "自动",
-  //     layoutName: "force",
-  //     layoutClassName: "seeks-layout-center",
-  //   }
-  // ]
-}
+  layouts: [
+    centerLayout,
+    treeLayout,
+    forceLayout,
+  ]
+};
 
 export default relationGraphConfig;
