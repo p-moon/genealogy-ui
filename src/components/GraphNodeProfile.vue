4f33a37fd45e6396e438fde7aac3968a295a6414
p-moon <moon@jdk.plus>
2023-05-04 10:49:07 +0800

    调整已有逻辑

--- src/components/GraphNodeProfile.vue
+++ src/components/GraphNodeProfile.vue
@@ -3,12 +3,72 @@
   <div v-if="isShowNodeMenuPanel" v-bind:class="{ 'node-menu-panel': true }"
        v-bind:style="{left: nodeMenuPanelPosition.x + 'px',top: nodeMenuPanelPosition.y + 'px'}">
     <div>对这个节点进行操作：</div>
-    <el-button type="primary" @click.stop="editNode">
-      <el-icon>
-        <Edit />
-      </el-icon>
-      编辑
-    </el-button>
+    <div>
+      <el-form
+        label-width="100px"
+        :model="currentNode"
+        style="max-width: 460px"
+      >
+        <el-form-item label="id">
+          <el-input v-model="currentNode.id" disabled />
+        </el-form-item>
+        <el-form-item label="名称">
+          <el-input v-model="currentNode.text" />
+        </el-form-item>
+        <el-form-item label="边框颜色">
+          <el-color-picker v-model="currentNode.borderColor" size="large" />
+        </el-form-item>
+        <el-form-item label="字体颜色">
+          <el-color-picker v-model="currentNode.fontColor" size="large" />
+        </el-form-item>
+        <!--            <el-form-item label="背景颜色">-->
+        <!--              <el-color-picker v-model="currentNode.color" size="large" />-->
+        <!--            </el-form-item>-->
+        <el-form-item label="透明度">
+          <el-slider v-model="currentNode.opacity" :step="1" :max="100"/>
+        </el-form-item>
+        <!--            <el-form-item label="节点宽度">-->
+        <!--              <el-input-number v-model="currentNode.width" :step="1" :max="1000"/>-->
+        <!--            </el-form-item>-->
+        <!--            <el-form-item label="节点高度">-->
+        <!--              <el-input-number v-model="currentNode.height" :step="1" :max="1000"/>-->
+        <!--            </el-form-item>-->
+        <!--            <el-form-item label="节点形状">-->
+        <!--              <el-select v-model="currentNode.nodeShape" class="m-2" placeholder="Select" size="large">-->
+        <!--                <el-option-->
+        <!--                  v-for="item in [{label:'圆形', value:0}, {label:'矩形', value:1}]"-->
+        <!--                  :key="item.value"-->
+        <!--                  :label="item.label"-->
+        <!--                  :value="item.value"-->
+        <!--                />-->
+        <!--              </el-select>-->
+        <!--            </el-form-item>-->
+        <!--            <el-form-item label="自定义节点内部属性">-->
+        <!--              <el-input-->
+        <!--                v-model="currentNode.innerHTML"-->
+        <!--                :rows="5"-->
+        <!--                type="textarea"-->
+        <!--                placeholder="用HTML自定义节点内部的内容，当此属性不为空时，text属性将失效"-->
+        <!--              />-->
+
+        <!--            </el-form-item>-->
+        <!--            <el-form-item label="自定义节点">-->
+        <!--              <el-input-->
+        <!--                v-model="currentNode.html"-->
+        <!--                :rows="5"-->
+        <!--                type="textarea"-->
+        <!--                placeholder="用HTML自定义节点，当此属性不为空时，节点的所有样式属性将失效"-->
+        <!--              />-->
+        <!--            </el-form-item>-->
+      </el-form>
+    </div>
+
+<!--    <el-button type="primary" @click.stop="editNode">-->
+<!--      <el-icon>-->
+<!--        <Edit />-->
+<!--      </el-icon>-->
+<!--      编辑-->
+<!--    </el-button>-->
     &nbsp;
     <el-dropdown size="default" split-button type="primary" @click.stop="addChildNode">
       添加子节点
@@ -75,6 +135,7 @@ export default defineComponent({
 
     function deleteNode() {
       relationGraphDelegate.deleteNode(currentNode.value);
+      hideNodeProfile();
     }
 
     function addLine(from:Node, to:Node) {
